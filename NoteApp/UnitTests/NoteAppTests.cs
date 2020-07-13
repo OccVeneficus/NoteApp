a79fbb77a0bfe5199025a4cd99f84e8b18736278
Nikolai Naberezhnev <supovik@gmail.com>
2020-07-13 11:55:39 +0700

    feature(NoteAppUnitTests): Создан проект для Юнит тестирования классов логики приложения
    - К проекту юнит тестов подключены библеотеки NUnit и NUnit3Adapter

---
+++ NoteApp/UnitTests/NoteAppTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class NoteTest
+    {
+
+    }
+
+    [TestFixture]
+    public class ProjectTest
+    {
+
+    }
+
+    [TestFixture]
+    public class ProjectmanagerTest
+    {
+
+    }
+}
